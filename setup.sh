024458ad4a7bcfbfd0a7eb11b9cfbeec7d8104fe
Jonas Schwartz <jonas@qutheory.io>
2018-05-21 19:52:33 +0200

    Updating CLI

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo gem install require tty-prompt paint colorize json redis netrc tty-spinner 
 if [ -d ~/vcloud-cli ]; then
 	rm -fr ~/vcloud-cli
 fi
-git clone git@github.com:vapor-cloud/vcloud-cli.git ~/vcloud-cli
+git clone https://github.com/vapor-cloud/vcloud-cli.git ~/vcloud-cli
 
 cp ~/vcloud-cli/vapor-alpha /usr/local/bin/vapor-alpha
 chmod +x /usr/local/bin/vapor-alpha
